f1e2266bcd0adc3d2b67307c7b7402d9b221f03e
André IKOTO <126103789+ikoto92@users.noreply.github.com>
2026-05-15 14:23:02 +0200

    Update CLS_Attack.cs

--- TP-Personnages/CLS_Attack.cs
+++ TP-Personnages/CLS_Attack.cs
@@ -27,7 +27,6 @@ namespace TP_Personnages
                         return personnage1;
                     }
                 }
-                // Si aucun personnage n'est à court de points de vie après 5 rounds, on décide du gagnant en fonction des points de vie restants
                 return personnage1.PointsDeVie > personnage2.PointsDeVie ? personnage1 : personnage2;
             }
         }      
